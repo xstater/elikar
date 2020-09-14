f17d120d746e67545216bd79c3523d6c5725a61b
xstater <1209635268@qq.com>
2020-09-14 23:05:37 +0800

    added sdl_test() test
    Signed-off-by: me <l>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn sdl_test() {
+        extern crate sdl2;
+
+        use sdl2::event::Event;
+        use sdl2::keyboard::Keycode;
+
+        let sdl_context = sdl2::init().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
+
+        let mut window = video_subsystem
+            .window("test",1280,768)
+            .opengl()
+            .build()
+            .unwrap();
+        window.show();
+
+        let mut event_pump = sdl_context
+            .event_pump()
+            .unwrap();
+
+        'mainloop: loop{
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit {..} | Event::KeyDown { keycode : Some(Keycode::Escape),..} => {
+                        break 'mainloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
     }
 }
