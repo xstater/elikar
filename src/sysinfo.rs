86f3b93dac8cf81c7d02d6298738247ee7e2d2f6
me <l>
2020-09-16 17:30:16 +0800

    added sysinfo module
    Signed-off-by: me <l>

--- src/sysinfo.rs
+++ src/sysinfo.rs
@@ -17,15 +17,92 @@ pub fn get_cpu_cache_line_size() -> i32{
 }
 
 #[inline]
-pub fn get_cpu_count() -> i32{
+pub fn get_cpu_count() -> u32{
     unsafe {
-        sdl2_sys::SDL_GetCPUCount()
+        sdl2_sys::SDL_GetCPUCount() as u32
+    }
+}
+
+pub type MiB = u32;
+#[inline]
+pub fn get_system_ram() -> MiB{
+    unsafe {
+        sdl2_sys::SDL_GetSystemRAM() as u32
     }
 }
 
 #[inline]
-pub fn get_system_ram() -> i32{
+pub fn has_3d_now() -> bool{
     unsafe {
-        sdl2_sys::SDL_GetSystemRAM()
+        sdl2_sys::SDL_Has3DNow() == sdl2_sys::SDL_bool::SDL_TRUE
+    }
+}
+
+#[inline]
+pub fn has_avx() -> bool{
+    unsafe {
+        sdl2_sys::SDL_HasAVX() == sdl2_sys::SDL_bool::SDL_TRUE
+    }
+}
+
+#[inline]
+pub fn has_avx2() -> bool{
+    unsafe {
+        sdl2_sys::SDL_HasAVX2() == sdl2_sys::SDL_bool::SDL_TRUE
+    }
+}
+
+#[inline]
+pub fn has_alti_vec() -> bool{
+    unsafe {
+        sdl2_sys::SDL_HasAltiVec() == sdl2_sys::SDL_bool::SDL_TRUE
+    }
+}
+
+
+#[inline]
+pub fn has_mmx() -> bool{
+    unsafe {
+        sdl2_sys::SDL_HasMMX() == sdl2_sys::SDL_bool::SDL_TRUE
+    }
+}
+
+
+#[inline]
+pub fn has_rdtsc() -> bool{
+    unsafe {
+        sdl2_sys::SDL_HasRDTSC() == sdl2_sys::SDL_bool::SDL_TRUE
+    }
+}
+
+
+#[inline]
+pub fn has_sse() -> bool{
+    unsafe {
+        sdl2_sys::SDL_HasSSE() == sdl2_sys::SDL_bool::SDL_TRUE
+    }
+}
+#[inline]
+pub fn has_sse2() -> bool{
+    unsafe {
+        sdl2_sys::SDL_HasSSE2() == sdl2_sys::SDL_bool::SDL_TRUE
+    }
+}
+#[inline]
+pub fn has_sse3() -> bool{
+    unsafe {
+        sdl2_sys::SDL_HasSSE3() == sdl2_sys::SDL_bool::SDL_TRUE
+    }
+}
+#[inline]
+pub fn has_sse41() -> bool{
+    unsafe {
+        sdl2_sys::SDL_HasSSE41() == sdl2_sys::SDL_bool::SDL_TRUE
+    }
+}
+#[inline]
+pub fn has_sse42() -> bool{
+    unsafe {
+        sdl2_sys::SDL_HasSSE42() == sdl2_sys::SDL_bool::SDL_TRUE
     }
 }
