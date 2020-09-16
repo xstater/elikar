8d14c44bd5c99e41a862451089558390eb3b0ee8
me <l>
2020-09-17 01:14:44 +0800

    added window creating test
    Signed-off-by: me <l>

--- tests/elikar_tests.rs
+++ tests/elikar_tests.rs
@@ -8,4 +8,10 @@ fn elikar_test(){
     let ek = Elikar::new()
         .unwrap();
 
+    #[allow(unused_variables)]
+    let window = elikar::window::Window::new("Fuck Rust",100,100,1280,800)
+        .unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
 }
