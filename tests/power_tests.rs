75cb0034a0d8f6af7ffbea59125f2f9978eb9271
xstater <1209635268@qq.com>
2020-11-02 01:27:37 +0800

    fixed indent
    Signed-off-by: me <l>

--- tests/power_tests.rs
+++ tests/power_tests.rs
@@ -5,5 +5,4 @@ fn power_test(){
     println!("Power state:{:?}",elikar::power::get_power_state());
     println!("Battery life time: {:?} second",elikar::power::get_battery_time());
     println!("Battery percentage:{:?} %",elikar::power::get_battery_percentage());
-
 }
