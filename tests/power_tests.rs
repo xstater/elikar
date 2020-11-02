295ebe406caaa309876a3390b98d47a8165468a7
xstater <1209635268@qq.com>
2020-11-03 02:16:51 +0800

    changed test
    Signed-off-by: me <l>

--- tests/power_tests.rs
+++ tests/power_tests.rs
@@ -1,8 +1,10 @@
 extern crate elikar;
 
+use elikar::common::unit::Second;
+
 #[test]
 fn power_test(){
     println!("Power state:{:?}",elikar::power::power_state());
-    println!("Battery life time: {:?} second",elikar::power::battery_time());
+    println!("Battery life time: {:?} second",elikar::power::battery_time().unwrap_or(0_i32.s()).as_s());
     println!("Battery percentage:{:?} %",elikar::power::battery_percentage());
 }
