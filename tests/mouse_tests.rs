9658f633da00edb8da868c744cf5825c96042af9
xstater <1209635268@qq.com>
2020-11-09 03:14:27 +0800

    removed bad code
    Signed-off-by: me <l>

--- tests/mouse_tests.rs
+++ tests/mouse_tests.rs
@@ -1,16 +1,5 @@
 extern crate elikar;
 
-use elikar::window::WindowsManager;
-
 #[test]
 fn mouse_test(){
-    let game = elikar::elikar::Elikar::new().unwrap();
-    let mut wm = WindowsManager::new(&game);
-    let window = wm.window_builder()
-        .title("mouse_test")
-        .size(1280,900)
-        .opengl()
-        .build_mut()
-        .unwrap();
-
 }
