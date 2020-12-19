d85cee9e8d1771326d4f51b86460f4a396e989f2
xstater <1209635268@qq.com>
2020-12-19 14:44:49 +0800

    add quit signal test

--- examples/elikar_tests.rs
+++ examples/elikar_tests.rs
@@ -2,7 +2,7 @@ extern crate elikar;
 
 use elikar::elikar::Elikar;
 use elikar::window;
-use elikar::system_event::Handlers;
+use elikar::system_event::Signals;
 
 fn main(){
     let mut game = Elikar::new().unwrap();
@@ -17,8 +17,12 @@ fn main(){
         .unwrap();
     wm.add_windows(window);
 
-    let mut event_handlers = Handlers::new();
+    let mut event_handlers = Signals::new();
 
+    let mut game_closure = game.clone();
+    event_handlers.quit.connect(move |()|{
+        game_closure.quit();
+    });
     event_handlers.mouse_button_down.connect(|(x,y)|{
         println!("({},{})",x,y);
     });
@@ -29,6 +33,5 @@ fn main(){
         }
     });
 
-    // elikar::elikar::run(game,event_handlers);
     game.run(event_handlers);
 }
