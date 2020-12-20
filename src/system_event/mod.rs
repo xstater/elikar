d19efb68cd97f452d97fd4f2e635c396de30d311
xstater <a166224@163.com>
2020-12-20 13:13:05 +0800

    dasdasdsadasdsdasdas

--- src/system_event/mod.rs
+++ src/system_event/mod.rs
@@ -23,22 +23,22 @@ impl Signals {
         }
     }
 
-    pub(in crate) fn dispatch(&mut self,sdlevesnt : SDL_Event){
-        match unafe { sdlevent.type_ } {
+    pub(in crate) fn dispatch(&mut self,sdl_event: SDL_Event){
+        match unsafe { sdl_event.type_ } {
             x if x == SDL_EventType::SDL_QUIT as u32 => {
                 self.quit.emit(());
             }
             x if x == SDL_EventType::SDL_MOUSEBUTTONDOWN as u32 => {
-                self.mouse_button_down.emit(unsafe{sdlevent.button}.into());
+                self.mouse_button_down.emit(unsafe{sdl_event.button}.into());
             }
             x if x == SDL_EventType::SDL_MOUSEBUTTONUP as u32 => {
-                self.mouse_button_up.emit(unsafe{sdlevent.button}.into());
+                self.mouse_button_up.emit(unsafe{sdl_event.button}.into());
             }
             x if x == SDL_EventType::SDL_MOUSEMOTION as u32 => {
-                self.mouse_motion.emit(unsafe{sdlevent.motion}.into());
+                self.mouse_motion.emit(unsafe{sdl_event.motion}.into());
             }
             x if x == SDL_EventType::SDL_MOUSEWHEEL as u32 => {
-                self.mouse_wheel.emit(unsafe{sdlevent.wheel}.into());
+                self.mouse_wheel.emit(unsafe{sdl_event.wheel}.into());
             }
             _ => {}
         }
