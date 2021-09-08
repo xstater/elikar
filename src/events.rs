144b3e4cadeb72262e86b9fdeebf7fd9a5688cc5
xstater <a166224@163.com>
2021-09-09 07:23:10 +0800

    修改了部分field为Vec以支持一帧内处理多次事件； 添加了窗口相关的事件信息window

--- src/events.rs
+++ src/events.rs
@@ -1,24 +1,37 @@
 use sdl2_sys::*;
 use xecs::System;
-use crate::{mouse, keyboard, drop_file};
+use crate::{mouse, keyboard, drop_file,window};
 use std::ffi::CStr;
 use std::path::PathBuf;
 
 #[derive(Default)]
 pub struct PollEvents {
     pub quit : Option<()>,
-    pub mouse_button_down : Option<mouse::event::button::EventInfo>,
-    pub mouse_button_up : Option<mouse::event::button::EventInfo>,
-    pub mouse_motion : Option<mouse::event::motion::EventInfo>,
-    pub mouse_wheel : Option<mouse::event::wheel::EventInfo>,
-    pub key_down : Option<keyboard::event::EventInfo>,
-    pub key_up : Option<keyboard::event::EventInfo>,
+    pub mouse_button_down : Vec<mouse::event::button::EventInfo>,
+    pub mouse_button_up : Vec<mouse::event::button::EventInfo>,
+    pub mouse_motion : Vec<mouse::event::motion::EventInfo>,
+    pub mouse_wheel : Vec<mouse::event::wheel::EventInfo>,
+    pub key_down : Vec<keyboard::event::EventInfo>,
+    pub key_up : Vec<keyboard::event::EventInfo>,
     pub drop_files : Option<drop_file::event::EventInfo>,
+    pub window : Vec<window::event::EventInfo>
 }
 
 impl PollEvents {
     pub fn new() -> PollEvents {
         PollEvents::default()
+    }
+
+    fn clear(&mut self){
+        self.quit = Option::None;
+        self.mouse_button_down.clear();
+        self.mouse_button_up.clear();
+        self.mouse_motion.clear();
+        self.mouse_wheel.clear();
+        self.key_down.clear();
+        self.key_up.clear();
+        self.drop_files = Option::None;
+        self.window.clear()
     }
 }
 
@@ -27,7 +40,7 @@ impl<'a> System<'a> for PollEvents {
     type Dependencies = ();
 
     fn update(&'a mut self, _ : ()) {
-        std::mem::swap(self,&mut PollEvents::default());
+        self.clear();
 
         let mut sdl_event = SDL_Event {type_:0};
         while unsafe { SDL_PollEvent(&mut sdl_event) } == 1 {
@@ -36,22 +49,22 @@ impl<'a> System<'a> for PollEvents {
                     self.quit = Some(())
                 }
                 x if x == SDL_EventType::SDL_MOUSEBUTTONDOWN as u32 => {
-                    self.mouse_button_down = Some(unsafe {sdl_event.button}.into());
+                    self.mouse_button_down.push(unsafe {sdl_event.button}.into());
                 }
                 x if x == SDL_EventType::SDL_MOUSEBUTTONUP as u32 => {
-                    self.mouse_button_up = Some(unsafe {sdl_event.button}.into());
+                    self.mouse_button_up.push(unsafe {sdl_event.button}.into());
                 }
                 x if x == SDL_EventType::SDL_MOUSEMOTION as u32 => {
-                    self.mouse_motion = Some(unsafe {sdl_event.motion}.into());
+                    self.mouse_motion.push(unsafe {sdl_event.motion}.into());
                 }
                 x if x == SDL_EventType::SDL_MOUSEWHEEL as u32 => {
-                    self.mouse_wheel = Some(unsafe {sdl_event.wheel}.into());
+                    self.mouse_wheel.push(unsafe {sdl_event.wheel}.into());
                 }
                 x if x == SDL_EventType::SDL_KEYDOWN as u32 => {
-                    self.key_down = Some(unsafe {sdl_event.key}.into());
+                    self.key_down.push(unsafe {sdl_event.key}.into());
                 }
                 x if x == SDL_EventType::SDL_KEYUP as u32 => {
-                    self.key_up = Some(unsafe {sdl_event.key}.into());
+                    self.key_up.push(unsafe {sdl_event.key}.into());
                 }
                 x if x == SDL_EventType::SDL_DROPFILE as u32 => {
                     let path = PathBuf::from(unsafe{CStr::from_ptr(sdl_event.drop.file)}.to_str().unwrap());
@@ -65,6 +78,9 @@ impl<'a> System<'a> for PollEvents {
                     self.drop_files = Some(unsafe{sdl_event.drop}.into())
                 }
                 x if x == SDL_EventType::SDL_DROPCOMPLETE as u32 => {}
+                x if x == SDL_EventType::SDL_WINDOWEVENT as u32 => {
+                    self.window.push(unsafe { sdl_event.window }.into())
+                }
                 _ => {}
             }
         }
--- src/window/mod.rs
+++ src/window/mod.rs
@@ -1,5 +1,6 @@
 mod builder;
 mod window;
+pub mod event;
 
 pub use window::Window;
 pub use builder::Builder;
