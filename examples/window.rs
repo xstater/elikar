18cb0d987e8fdcaffb5a9a18ed8de3a7066d0631
xstater <a166224@163.com>
2021-09-09 07:25:02 +0800

    添加了对窗口相关事件的测试

--- examples/window.rs
+++ examples/window.rs
@@ -1,41 +1,44 @@
-extern crate elikar;
+use elikar::{Elikar, ElikarStates};
+use xecs::System;
+use elikar::events::PollEvents;
+use std::cell::{Ref, RefMut};
 
+struct QuitSystem;
+impl<'a> System<'a> for QuitSystem {
+    type Resource = (&'a PollEvents,&'a mut ElikarStates);
+    type Dependencies = PollEvents;
+
+    fn update(&'a mut self, (events,mut states) : (Ref<'a,PollEvents>,RefMut<'a,ElikarStates>)) {
+        if let Some(_) = events.quit {
+            states.quit()
+        }
+    }
+}
+
+struct PrintWindowEvent;
+impl<'a> System<'a> for PrintWindowEvent {
+    type Resource = &'a PollEvents;
+    type Dependencies = PollEvents;
+
+    fn update(&'a mut self,events : Ref<'a,PollEvents>) {
+        for event in &events.window {
+            println!("Window Event:{:?}", event)
+        }
+    }
+}
 fn main() {
-    use elikar::elikar::Elikar;
+    let mut game = Elikar::new().unwrap();
 
-    let ek = Elikar::new().unwrap();
-
-    let window = ek.create_window()
-        .title("测试窗口")
-        .position_centred()
-        .size(1280, 700)
-        // .maximized()
-        .opengl()
+    let mut window = game.create_window()
         .build()
         .unwrap();
+    window.set_resizable();
 
-    // window.set_size(1288,888);
-    println!("size:{:?}", window.size());
-    // window.set_position(200,200);
-    println!("position:{:?}", window.position());
-    // window.set_brightness(0.5).unwrap();
-    println!("brightness:{}", window.brightness());
-    // window.set_opacity(0.5).unwrap();
-    println!("opacity:{:?}", window.opacity().unwrap());
-    // window.set_title("shit").unwrap();
-    println!("title:{}", window.title());
+    game.current_stage_mut()
+        .add_system(QuitSystem)
+        .add_system(PollEvents::new())
+        .add_system(PrintWindowEvent);
 
-    let id = window.id().unwrap();
-    println!("id:{}",id);
-    // window.hide();
-    // window.show();
-    // window.maximize();
-    // window.minimize();
-    // window.raise();
-    // window.restore();
-    // window.set_fullscreen().unwrap();
-    // window.set_fullscreen_desktop().unwrap();
-
-    std::thread::sleep(std::time::Duration::from_secs(1));
+    game.run();
 }
 
